7e2fa5deabb5a42c54a0d3454e41dde7f07ed3cc
srour <thomas.srour@epfl.ch>
2022-04-13 15:54:34 +0300

    fixes3 : step 1

--- new-scenario-template/background1.sh
+++ new-scenario-template/background1.sh
@@ -1,6 +1,6 @@
-mkdir /root/project/First_Try
+mkdir -p /root/project/First_Try
 touch project/First_Try/Dockerfile
 echo "#This is where you write your first Dockerfile" >> project/First_Try/Dockerfile
-mkdir /root/project/Second_Try
+mkdir -p /root/project/Second_Try
 touch project/Second_Try/Dockerfile
 echo "#This is where you write your second Dockerfile" >> project/Second_Try/Dockerfile
